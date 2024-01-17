ebdf573471e420e4bdf317e21b6ce9c7a6900170
fmnb0516 <fmnb0516@gmail.com>
2024-01-17 14:40:31 +0900

    update

--- tools/battle/index.js
+++ tools/battle/index.js
@@ -85,7 +85,7 @@ const run = async() => {
     for (let i = 0; i < targets.length; i++) {
         const user = targets[i];
 
-        if(user.nick_no === '57064765') {
+        if(user.nick_no === '67703296') {
             startflg = true;
         }
         if(startflg === false) {
